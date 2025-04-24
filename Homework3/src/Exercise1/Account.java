83816dc2936bbb7f28ae24ab384432c2e6574da2
Kilian L. <klipinsk@smail.uni-koeln.de>
2025-04-24 10:19:07 +0200

    Homework3 gemacht und fertig, konstruktur hinzugefügt

--- Homework3/src/Exercise1/Account.java
+++ Homework3/src/Exercise1/Account.java
@@ -6,9 +6,16 @@ public class Account {
     protected int pin;
     String internalNote;
 
+    public Account(String owner, double balance, int pin, String internalNote) {
+        this.owner = owner;
+        this.balance = balance;
+        this.pin = pin;
+        this.internalNote = internalNote;
+    }
     public double getBalance(){
         return balance;
     }
+
     public boolean changePin(int currentPin, int newPin){
         if(this.pin == currentPin){
             this.pin = newPin;
--- Homework3/src/Exercise1/Exercise1diffrent2/CompanyAccount.java
+++ Homework3/src/Exercise1/Exercise1diffrent2/CompanyAccount.java
@@ -3,5 +3,7 @@ package Exercise1.Exercise1diffrent2;
 import Exercise1.Account;
 
 public class CompanyAccount extends Account {
-
+    public CompanyAccount(String owner, double balance, int pin, String internalNote) {
+        super(owner, balance, pin, internalNote);
+    }
 }
